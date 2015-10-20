173ac97a3c29cf65192a4bacd89d35bac94162a4
curogihu <curoas@yahoo.co.jp>
2015-10-19 20:00:53 -0700

    Delete almost comment
    I have to do refactoring, in particular avoiding nested for each
    statement.

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
 <?php
 
-// the following code is including other php file.
-// require_one('php file name');
+echo "Start: " . date("H:i:s") . "<br><br>";
 
-echo "Start: " . date("H:i:s");
-
-
-// this code will be comment out after creating.
 ini_set('display_errors', 'On');
-
 $dsn = 'mysql:dbname=LoLItemHistory;host=localhost';
 $user = 'root';
 $password = 'root';
@@ -25,7 +19,6 @@ try{
 date_default_timezone_set("Asia/Tokyo");
 
 try{
-
   $results = $dbh->query('SELECT * FROM LoLChampion WHERE championId BETWEEN 0 AND 2 ORDER BY championId');
   $championDataArr = $results->fetchAll(PDO::FETCH_ASSOC);
 
@@ -37,40 +30,31 @@ try{
 //inctioのライブラリ呼び出し
 include_once('IXR_Library.php');
 require_once 'simplehtmldom/simple_html_dom.php';
-
 $stmt = $dbh->prepare("DELETE FROM LoLItem");
 $stmt->execute();
 
+$id = 0;
+
 foreach($championDataArr as $championData){
-  // I have to add each character name when searching.
-/*  
-  $baseUrl = "http://www.probuilds.net/champions/";
-  $characterName = "Garen";
-  $pageData = mb_convert_encoding(file_get_contents($baseUrl . $characterName),'UTF-8','auto');
-*/
   $pageData = mb_convert_encoding(file_get_contents($championData["championUrl"]),'UTF-8','auto');
   $html = str_get_html($pageData);
 
   // If using local html file
   //$html = file_get_html('sample.html');
 
-  $id = 0;
   $championId = $championData["championId"];
   $record = 0;
 
   foreach($html->find('div[class=block]') as $buildRecord){
-
     $itemRecord = 0;
 
     foreach($buildRecord->find('img') as $item){
-
       $itemFlg = strpos($item->src, "http://www.probuilds.net/resources/img/items/");
       $emptyFlg = strpos($item->src, "EmptyIcon.png");
 
       if($itemFlg !== false && $emptyFlg === false){
         // Without conversing single quotation, couldn't insert record.
         $itemName = str_replace("'", "\'", $item->alt);
-
         $stmt = $dbh->prepare("INSERT INTO LoLItem (id, championId, record, item, name) VALUES (?, ?, ?, ?, ?)");
         $stmt->bindParam(1, $id);
         $stmt->bindParam(2, $championId);
@@ -78,10 +62,8 @@ foreach($championDataArr as $championData){
         $stmt->bindParam(4, $itemRecord);
         $stmt->bindParam(5, $itemName);
         $stmt->execute();
-
         $id++;
         $itemRecord++;
-
       }
     }
 
@@ -91,8 +73,4 @@ foreach($championDataArr as $championData){
   echo "ChampionId: " . $championId . ", Start: " . date("H:i:s") . "<br>";
 }
 
-echo "End: " . date("H:i:s");
-//<div class='block alt'>
-//foreach($html->find('div[class=property  js-property js-cassetLink]') as $house){
-
-//echo ($cnt);
+echo "<br>End: " . date("H:i:s");
