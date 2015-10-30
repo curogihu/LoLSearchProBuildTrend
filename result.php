6c81a22b370dfab2f14e526a773369ada7b2beb2
curogihu <curoas@yahoo.co.jp>
2015-10-30 15:41:04 -0700

    Writing crucial logic is finished
    Left process:
    1. Add error check
    2. Accept responsive design
    3. Add Image file

--- result.php
+++ result.php
@@ -30,6 +30,15 @@ try{
   $results = $dbh->query("SELECT * FROM LoLChampion ORDER BY championId");
   $championDataArr = $results->fetchAll(PDO::FETCH_ASSOC);
 
+  if(!empty($_SESSION["targetChampionId"])){
+    $buildResults = $dbh->query(getSQLForResearchBuild($_SESSION["targetChampionId"]));
+    $buildArr = $buildResults->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+
+//getSQLForResearchBuild($_SESSION["targetChampionId"])
+  //echo "string = " . getSQLForResearchBuild($_SESSION["targetChampionId"]);
+
 }catch(Exception $e){
   echo $e->getMessage();
   die();
@@ -59,7 +68,13 @@ try{
       </form>
     </div>
 
+    <?php 
+      echo createBuildList($buildArr);
+     ?>
+
+
     <div id="resultList">
+<!--
       <p>00-05min</p>
       <table border="1">
         <tr>
@@ -150,6 +165,7 @@ try{
         </tr>
       </table>
     </div>
+-->
   </div>
 
   <div id="footer">
@@ -176,7 +192,71 @@ function createOptionList($championArr){
   }
 }
 
+function createBuildList($buildArr){
+
+  $outputStr = "<table>";
+
+  foreach ($buildArr as $buildRecord) {
+    $outputStr .= "<tr>";
+    $outputStr .="<td>" . $buildRecord["periodCategory"]. "</td>";
+    $outputStr .="<td>" . $buildRecord["displayItemName"]. "</td>";
+    $outputStr .="<td>" . $buildRecord["displayItemImagePath"]. "</td>";
+    $outputStr .="</tr>";
+  }
+
+  return $outputStr . "</table>";
+}
+
+function getSQLForResearchBuild($championId){
+  return "select accumTable.periodCategory, " .
+          "accumTable.itemId, " .
+          "accumTable.displayItemName, " .
+          "accumTable.displayItemImagePath, " .
+          "count(accumTable.itemId) as frequent " .
+          "from (" .
+          "SELECT CASE WHEN lbh.elapsedTime <= 300000 THEN '00-05min' " .
+          "WHEN lbh.elapsedTime between 300001 and 600000 THEN '05-10min' " .
+          "WHEN lbh.elapsedTime between 600001 and 1200000 THEN '10-20min' " .
+          "WHEN lbh.elapsedTime between 1200001 and 1800000 THEN '20-30min' " .
+          "ELSE '30-XXmin' " .
+          "END AS periodCategory, " .
+          "lbh.itemId, " .
+          "IFNULL(li.itemName, '') as displayItemName, " .
+          "IFNULL(li.itemImagePath, '') as displayItemImagePath " .
+          "FROM LoLBuildHistory lbh LEFT JOIN LoLItem li ON lbh.itemId = li.itemId " .
+          "WHERE championId = " .  $championId . " " .
+          ") as accumTable " .
+          "group by accumTable.periodCategory, " .
+          "accumTable.itemId, " .
+          "accumTable.displayItemName, " .
+          "accumTable.displayItemImagePath " .
+          "order by accumTable.periodCategory asc, " .
+          "frequent desc, " .
+          "accumTable.itemId asc ";
+}
 
 /*
-select lbh.championId, lbh.buildId, lbh.elapsedTime, lbh.itemid, li.itemName from LoLBuildHistory lbh left join LoLItem li on lbh.itemId = li.itemId
+select accumTable.periodCategory, accumTable.itemId, accumTable.displayItemName, accumTable.displayItemImagePath, count(accumTable.itemId) as frequent
+from
+(
+SELECT case when lbh.elapsedTime <= 300000 then '00-05min'
+            when lbh.elapsedTime between 300001 and 600000 then '05-10min'
+            when lbh.elapsedTime between 600001 and 1200000 then '10-20min'
+            when lbh.elapsedTime between 1200001 and 1800000 then '20-30min'
+            else '30-XXmin'
+       end as periodCategory,
+       lbh.itemId,
+       ifnull(li.itemName, "") as displayItemName,
+       ifnull(li.itemImagePath, "") as displayItemImagePath
+FROM LoLBuildHistory lbh left join LoLItem li on lbh.itemId = li.itemId
+where championId = 21
+) as accumTable
+group by accumTable.periodCategory, accumTable.itemId, accumTable.displayItemName, accumTable.displayItemImagePath
+order by accumTable.periodCategory, frequent desc, accumTable.itemId
+*/
+
+
+
+/*
+select lbh.championId, lbh.buildId, lbh.elapsedTime, lbh.itemid, li.itemName, ifnull(li.itemImagePath,"") as displayItemImagePath from LoLBuildHistory lbh left join LoLItem li on lbh.itemId = li.itemId
 */
